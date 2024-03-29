f65883cfeceeb5c01e114cef83ac89d1ad854343
Haroon-Niamat <116777848+Haroon-Niamat@users.noreply.github.com>
2024-03-30 00:45:14 +0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -39,3 +39,13 @@ curl -fsSL https://deb.nodesource.com/gpgkey/nodesource.gpg.key | sudo gpg --dea
 echo "deb [signed-by=/usr/share/keyrings/nodesource-archive-keyring.gpg] https://deb.nodesource.com/node_16.x focal main" | sudo tee /etc/apt/sources.list.d/nodesource.list
 sudo apt update
 sudo apt install -y nodejs
+
+# install docker compose 
+sudo apt update
+sudo apt install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update
+sudo apt install docker-compose-plugin -y
